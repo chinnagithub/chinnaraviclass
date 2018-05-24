5bc06858e1d11780f690bf058e7a12bebcfe4510
Ravi <chinnababumatsa@gmail.com>
2018-05-24 10:03:35 +0530

    first commit

---
+++ src/main/java/com/cr/chinnaravi/crclass.java
@@ -0,0 +1,5 @@
+package com.cr.chinnaravi;
+
+public class crclass {
+
+}
---
+++ src/test/java/com/cr/chinnaravi/crtest.java
@@ -0,0 +1,7 @@
+package com.cr.chinnaravi;
+
+import junit.framework.TestCase;
+
+public class crtest extends TestCase {
+
+}
